2edf9dcc3184451ccce80e9574d9e4e8122cc49b
luismiguel0406 <51542588+luismiguel0406@users.noreply.github.com>
2020-11-03 00:57:04 -0400

    Primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'
+$(document).ready(function(){
+
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        responsive:true,
+        slideWidth: 1280
+        
+        
+      })
+});
+
